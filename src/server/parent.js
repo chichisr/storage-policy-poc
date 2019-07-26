44abcb64e1e3f5a47c731e2ca37e0251b591652d
Chi-chi Wang <cwang@shoprunner.com>
2019-07-26 16:27:51 -0500

    [assets] Add parent index.html (Hello World)
    [package] Add dependency: express
    [src:server] Add server: parent.js

---
+++ src/server/parent.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const http = require('http');
+
+const parentAppPath = path.join(__dirname, '..', '..', 'assets', 'index.html');
+
+const port = {
+    parent: 8080,
+};
+
+/**
+ * Create the express apps
+ * Set them to serve static assets from the assets directories
+ */
+const parentApp = express();
+
+parentApp.get('*', (req, res) => {
+    fs.readFile(parentAppPath, { encoding: 'utf-8' }, (err, data) => {
+        if (err) throw err;
+        res.send(data);
+    });
+});
+
+const parentServer = http.createServer(parentApp);
+
+parentServer.listen(port.parent);
+console.log(`parentServer running on http://localhost:${port.parent}...`);
