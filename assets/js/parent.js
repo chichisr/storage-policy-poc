202db98c34c9da48900ced3daf72d087a8cf4d94
Jose Bucio <jbucio@shoprunner.com>
2019-08-05 10:22:15 -0500

    [index] added button to render component [assets/js/parent] added event listener to button click and trigger popup instead of iframe

--- assets/js/parent.js
+++ assets/js/parent.js
@@ -1,9 +1,20 @@
 // Top-level domain script
+console.log('[DEBUG parent.js]: cookies BEFORE write', document.cookie);
 const iframe = zoid.create({
     tag: 'storage',
     url: 'http://localhost:8000',
+    contexts: {
+        iframe: false,
+        popup: true,
+    },
 });
 
-iframe.render({}, '#root');
+document.addEventListener('DOMContentLoaded', () => {
+    const $showChildButton = document.getElementById('showChild');
+    $showChildButton.addEventListener('click', (e) => {
+        iframe.render({}, '#root');
+    });
+});
 
 document.cookie = 'parent=true';
+console.log('[DEBUG parent.js]: cookies AFTER write', document.cookie);
